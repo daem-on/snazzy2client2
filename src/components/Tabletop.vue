c5f5732a0c4e887cd9977d25c6f3cf782a5c1bac
Csongor Zih <daemon.onwave@gmail.com>
2024-03-07 22:20:55 +0100

    Add hidden cards to tabletop

--- src/components/Tabletop.vue
+++ src/components/Tabletop.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, reactive, ref, watch } from "vue";
+import { computed, ref, watch } from "vue";
 import { PlayerStatus } from '../../server2/dtos';
 import type { GameStateSlice } from '../../server2/dtos';
 import Card from "./Card.vue";
@@ -13,28 +13,26 @@ const emit = defineEmits<{
 	(e: "pickCard", index: number): void;
 }>();
 
-const state = reactive({
-	call: props.state.call,
-	revealedResponses: props.state.revealedResponses,
-	reveal: props.state.revealedResponses != null
-});
 const localWinner = ref<number | undefined>(undefined);
 
 watch(() => props.state, () => {
 	if (props.state.lastWinner) localWinner.value = undefined;
-	state.call = props.state.call;
-	state.revealedResponses = props.state.revealedResponses;
-	state.reveal = props.state.revealedResponses != null;
 });
+
+const reveal = computed(() => props.state.revealedResponses != null);
 
 const realWinner = computed(() => 
 	props.state.lastWinner?.revealIndex
 );
 
+const finishedPlayerCount = computed(() => 
+	props.state.players.filter(p => p.status === PlayerStatus.Finished).length
+);
+
 function pickCard(index: number) {
 	if (
 		props.myStatus !== PlayerStatus.Picking
-		|| !state.reveal
+		|| !reveal.value
 		|| realWinner.value != null
 	) return;
 	emit("pickCard", index);
@@ -46,18 +44,23 @@ function pickCard(index: number) {
 <template>
 	<div id="tabletop" class="flex flex-row flex-nowrap justify-start outline-none overflow-x-auto gap-4 flex-shrink-0 px-7 py-5 max-w-full">
 		<Card class="flex-shrink-0" v-if="state.call != undefined" :id="state.call" type="black" />
-		<div v-if="state.revealedResponses?.length" class="flex-shrink-0 flex flex-row gap-4">
-			<Card
-				v-for="(response, index) in state.revealedResponses"
-				class="white card"
-				@click="pickCard(index)"
-				:key="index"
-				:id="state.call!"
-				:winner="realWinner === index || localWinner === index"
-				:interpolate-ids="response"
-				:hide="false"
-				type="played"
-				/>
+		<div class="flex-shrink-0 flex flex-row gap-4">
+			<template v-if="state.revealedResponses?.length">
+				<Card
+					v-for="(response, index) in state.revealedResponses"
+					class="card"
+					@click="pickCard(index)"
+					:key="index"
+					:id="state.call!"
+					:winner="realWinner === index || localWinner === index"
+					:interpolate-ids="response"
+					:hide="false"
+					type="played"
+					/>
+			</template>
+			<template v-else>
+				<Card v-for="index in finishedPlayerCount" :key="index" :id="state.call!" :hide="true" type="played"></Card>
+			</template>
 		</div>
 	</div>
 </template>
